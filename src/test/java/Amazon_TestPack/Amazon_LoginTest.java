82350efe538c519f6346a173322757fe017a28a9
Dev-Bhagwani <132754891+Dev-Bhagwani@users.noreply.github.com>
2023-06-22 12:53:06 +0530

    Update Amazon_LoginTest.java

--- src/test/java/Amazon_TestPack/Amazon_LoginTest.java
+++ src/test/java/Amazon_TestPack/Amazon_LoginTest.java
@@ -24,44 +24,34 @@ public class Amazon_LoginTest extends Amazon_Base {
 		driver = initializeBrowser();
 		driver.get(prop.getProperty("url"));
 		
-		Amazon_LandingPage landingPage = new Amazon_LandingPage(driver);    // Amazon_LandingPage elements
-		Amazon_LoginPage loginPage = new Amazon_LoginPage(driver);    // Amazon_loginPage elements
-		Amazon_HomePage homePage = new Amazon_HomePage(driver);    // Amazon_HomePage elements
+		Amazon_LandingPage landingPage = new Amazon_LandingPage(driver);    
+		Amazon_LoginPage loginPage = new Amazon_LoginPage(driver);    
+		Amazon_HomePage homePage = new Amazon_HomePage(driver);    
 		Amazon_SearchPage  searchPage = new Amazon_SearchPage(driver);
 		Amazon_SelectingItems selectingItems = new Amazon_SelectingItems(driver);
 
-		//LandingPage
 		landingPage.myAccountDropdown().click();
 		
-		//LoginPage
 		loginPage.emailBox().sendKeys(prop.getProperty("email"));
 		loginPage.continueButton();
 		loginPage.passwordBox().sendKeys(prop.getProperty("password"));
 		loginPage.signInButton();
 		
 		Thread.sleep(5000);
-		Assert.assertTrue(	homePage.accountPage().isDisplayed());   // to dentoify whether entered the amazon correctly
+		Assert.assertTrue(homePage.accountPage().isDisplayed());   
 		
-		// SearchPage
 		searchPage.searchBox().click();
 		Thread.sleep(2000);
 		searchPage.searchBox().sendKeys(prop.getProperty("searchText"));
 		searchPage.searchButton().click();
-		
-		//SelectingItems
+			
 		selectingItems.OnepPlus().click();
 		Assert.assertTrue(selectingItems.withExchangePanel().isDisplayed());
 		System.out.println("passed");
-	
-		windowHandle();       // calling the Window Handle which is switching from one window to another window
-		
-		Thread.sleep(2000);
-		selectingItems.addToCart();
 	}
 	
 	@AfterMethod
 	public void closure() {
 		driver.quit();
 	}
-
 }
